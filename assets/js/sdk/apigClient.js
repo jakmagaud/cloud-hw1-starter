cb3c4921225f5c5f77f222035bd48d3c2affdaca
Anchit.Cap <anchit.cap@gmail.com>
2020-10-06 13:08:39 -0400

    Changes the api invoke url

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
 
     // extract endpoint and path from url
-    var invokeUrl = 'https://abc123.execute-api.us-east-1.amazonaws.com/v1';
+    var invokeUrl = 'https://fmgg8wnksf.execute-api.us-east-1.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
